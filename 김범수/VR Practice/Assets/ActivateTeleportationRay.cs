781e025f72c8c15570c9b1ab0acecf5cd9192a11
Starbow-Break <phsyobs@naver.com>
2024-06-11 10:36:30 +0900

    ADD : Tunneling Vignette 적용

--- 김범수/VR Practice/Assets/ActivateTeleportationRay.cs
+++ 김범수/VR Practice/Assets/ActivateTeleportationRay.cs
@@ -26,10 +26,10 @@ public class ActivateTeleportationRay : MonoBehaviour
     {
         // hover 상태가 아니고 select 입력이 발생하지 않으면서 activate 입력이 일정 수준 이상 들어오면 오브젝트 활성화
         // UI랑 상호작용 하거나 총을 쥐고 있는 상태에서는 텔레포트 오브젝트를 활성화 시키지 않게 하기 위함
-        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        bool isLeftRayHovering = leftRay ? leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid) : false;
         leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
 
-        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+        bool isRightRayHovering = rightRay ? rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid) : false;
         rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
